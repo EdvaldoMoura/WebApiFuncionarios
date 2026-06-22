6ad88a05618872c3e25f9584e2870c63e714b48c
Edvaldo Moura <88719849+EdvaldoMoura@users.noreply.github.com>
2026-06-22 13:24:23 -0300

    Delete WebApi directory

--- WebApi/Models/Response.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi.Models
-{
-    public class Response<Any>
-    {
-        public Any? Dados { get; set; }
-        public string Mensagem { get; set; } = string.Empty;
-        public bool Status { get; set; } = true;
-    }
-}
